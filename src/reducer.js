33826ebee8ab57edbabe3d35690db5418369ed7f
noordseoostende <voropayev1@mail.ru>
2022-02-20 20:13:57 +0100

    set_stories in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,7 @@ import { SET_LOADING, SET_STORIES, REMOVE_STORY, HANDLE_PAGE, HANDLE_SEARCH } fr
 
 const reducer = (state,action) => {
   switch(action.type){
-    case 'SET_LOADING':
+    case SET_LOADING:
       return {...state, isLoading:true}
       default:
         throw new Error (`no matching "${action.type}" action type`)
